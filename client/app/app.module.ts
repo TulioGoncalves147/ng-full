beb94089ec006001dc16370bbca825695e67a1a6
Tulio <tuliogoncalves5@gmail.com>
2017-12-10 19:58:29 -0200

    app

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -23,7 +23,6 @@ import { AccountComponent } from './account/account.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
